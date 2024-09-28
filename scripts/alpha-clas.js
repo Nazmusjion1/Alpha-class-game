1eb44032108d02b3bfef64b9b5156a1319e7efcc
Nazmusjion1 <nazmusjion5@gmail.com>
2024-09-29 00:46:18 +0600

    js addded

---
+++ scripts/alpha-clas.js
@@ -0,0 +1,11 @@
+function play(){
+//step-1: hide the home screen . to hide the screen add the  class hidden to home section
+  const homeSection = document.getElementById('home-screen');
+//   console.log(homeSection.classList);
+  homeSection.classList.add('hidden');
+
+// show the playground
+  const playGroundSection = document.getElementById('play-ground');
+//   console.log(playGroundSection.classList)
+    playGroundSection.classList.remove('hidden')
+}
