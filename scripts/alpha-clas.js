f0aa80f38f9f5468d2a574ba830b5c3e1a39a44d
Nazmusjion1 <nazmusjion5@gmail.com>
2024-09-29 00:54:59 +0600

    utility added

--- scripts/alpha-clas.js
+++ scripts/alpha-clas.js
@@ -1,11 +1,15 @@
-function play(){
-//step-1: hide the home screen . to hide the screen add the  class hidden to home section
-  const homeSection = document.getElementById('home-screen');
-//   console.log(homeSection.classList);
-  homeSection.classList.add('hidden');
+// function play(){
+// //step-1: hide the home screen . to hide the screen add the  class hidden to home section
+//   const homeSection = document.getElementById('home-screen');
+// //   console.log(homeSection.classList);
+//   homeSection.classList.add('hidden');
 
-// show the playground
-  const playGroundSection = document.getElementById('play-ground');
-//   console.log(playGroundSection.classList)
-    playGroundSection.classList.remove('hidden')
+// // show the playground
+//   const playGroundSection = document.getElementById('play-ground');
+// //   console.log(playGroundSection.classList)
+//     playGroundSection.classList.remove('hidden')
+// }
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
 }
