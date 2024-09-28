79037a9a4cd878e867e9dbbd40e7d55da238361c
Nazmusjion1 <nazmusjion5@gmail.com>
2024-09-29 01:35:12 +0600

    utility added

--- scripts/alpha-clas.js
+++ scripts/alpha-clas.js
@@ -9,7 +9,16 @@
 // //   console.log(playGroundSection.classList)
 //     playGroundSection.classList.remove('hidden')
 // }
+
+function continueGame(){
+    // step-1: generate random alphabet
+    const alphabet = getARandomAlphabet();
+    console.log('Your random alphabet', alphabet);
+
+}
+
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
+    continueGame();
 }
