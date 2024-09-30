19a9471003179161db2468be9af0487b8e300507
Nazmusjion1 <nazmusjion5@gmail.com>
2024-09-30 23:18:22 +0600

    score section updated

--- scripts/alpha-clas.js
+++ scripts/alpha-clas.js
@@ -12,6 +12,13 @@
 
 function handleKeyboardButtonPress(event) {
     const playerPressed = event.key;
+    console.log('Player pressed', playerPressed);
+
+    // stop the game if pressed 'Esc'
+    if(playerPressed === 'Escape'){
+        gameOver();
+    }
+
 
     console.log('Player Pressed', playerPressed);
 
@@ -61,6 +68,10 @@ function handleKeyboardButtonPress(event) {
         const currentLife = getTextElementValueById('current-life');
         const updatedLife = currentLife - 1;
         setTextElementValueById('current-life', updatedLife);
+
+        if(updatedLife === 0){
+           gameOver();
+        }
              
 
 
@@ -100,7 +111,29 @@ function continueGame() {
 }
 
 function play() {
+    // hide everything show only the playground
     hideElementById('home-screen');
+    hideElementById('final-score')
     showElementById('play-ground');
+
+    // reset score and life
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
     continueGame();
+}
+
+function gameOver(){
+     hideElementById('play-ground');
+     showElementById('final-score');
+    //  update final score
+    // 1. get the final score
+    const lastScore = getTextElementValueById('current-score');
+    console.log(lastScore);
+    setTextElementValueById('last-score',lastScore);
+
+    // clear the last selected highlight
+    const currentAlphabet = getElementTextById('current-alphabet');
+    // console.log(currentAlphabet);
+    removeBackgroundColorById(currentAlphabet);
+
 }
