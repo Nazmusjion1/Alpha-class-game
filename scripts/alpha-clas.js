47fbd07dc63ab1de3273c6014595ca6372deed74
Nazmusjion1 <nazmusjion5@gmail.com>
2024-09-30 20:09:12 +0600

    function added

--- scripts/alpha-clas.js
+++ scripts/alpha-clas.js
@@ -24,19 +24,30 @@ function handleKeyboardButtonPress(event) {
     // check matched or not
     if (playerPressed === expectedAlphabet) {
         console.log('You get a point');
-        console.log('You have pressed correctly', expectedAlphabet);
+        // console.log('You have pressed correctly', expectedAlphabet);
+           
+        const currentScore = getTextElementValueById('current-score');
+        const updatedScore = currentScore + 1;
+        setTextElementValueById('current-score', updatedScore);
+       
+
+
+
+        // ---------------------------------------------
         //    update score
         // 1.get the current score
-        const currentScoreElement = document.getElementById('current-score');
-        const currentScoreText = currentScoreElement.innerText;
-        const currentScore = parseInt(currentScoreText);
+        //    const currentScoreElement = document.getElementById('current-score');
+        //     const currentScoreText = currentScoreElement.innerText;
+        //     const currentScore = parseInt(currentScoreText);
+        //     console.log(currentScore);
+        // const currentScore = getTextElementValueById('current-score');
 
-        console.log(currentScore);
-        // 2.increase the score by 1
-        const newScore = currentScore + 1;
 
-        // show the update score
-       currentScoreElement.innerText = newScore;
+        //     // 2.increase the score by 1
+            const newScore = currentScore + 1;
+
+        //     // show the update score
+        //    currentScoreElement.innerText = newScore;
 
 
         // Start a new round
@@ -45,7 +56,28 @@ function handleKeyboardButtonPress(event) {
 
     }
     else {
-        console.log('you missed. you lost a life')
+        console.log('you missed. you lost a life');
+
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementValueById('current-life', updatedLife);
+             
+
+
+
+
+
+
+        // --------------------------------------
+        // step-1 get the current life number 
+        /* const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+      // step-2 reduce the life count
+       const newLife = currentLife - 1;
+
+      // step-3 display the updated life count
+      currentLifeElement.innerText = newLife; */
     }
 }
 // capture keyboard key press
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,24 +1,37 @@
-function hideElementById(elementId){
+function hideElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('hidden')
 }
 
 
-function showElementById(elementId){
+function showElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden')
 }
 
-function setBackgroundColor(elementId){
-      const element = document.getElementById(elementId);
-      element.classList.add('bg-orange-400');
-}
-function removeBackgroundColorById(elementId){
+function setBackgroundColor(elementId) {
     const element = document.getElementById(elementId);
-    element.classList.remove('bg-orange-400')
+    element.classList.add('bg-orange-400');
+}
+function removeBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
 }
 
-function getARandomAlphabet(){
+
+function getTextElementValueById(elementId){
+       const element = document.getElementById(elementId);
+       const elementValueText = element.innerText;
+       const value = parseInt(elementValueText);
+       return value;
+}
+
+function setTextElementValueById (elementId, value){
+        const element = document.getElementById(elementId);
+        element.innerText = value;
+}
+
+function getARandomAlphabet() {
     // get or create an alphabet array
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('');
@@ -27,7 +40,7 @@ function getARandomAlphabet(){
     // get a random index between 0-25
     const randomNumber = Math.random() * 25;
     const index = Math.round(randomNumber);
-    
+
 
     const alphabet = alphabets[index];
     // console.log(index, alphabet);
